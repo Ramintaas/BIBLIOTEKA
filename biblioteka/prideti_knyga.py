eebfaee0523c92f724133784c65ae0dcaa3841d9
Raminta_as <raminta.bartuliene@gmail.com>
2024-10-10 10:44:48 +0300

    Pridedame nauja knyga i bibliotekos klase.

---
+++ biblioteka/prideti_knyga.py
@@ -0,0 +1,45 @@
+import pickle
+from datetime import datetime
+
+class Knyga:
+    def __init__(self, autorius, pavadinimas, isleidimo_metai, zanras):
+        self.autorius = autorius
+        self.pavadinimas = pavadinimas
+        self.isleidimo_metai=isleidimo_metai
+        self.zanras = zanras
+
+    def __repr__(self):
+        return f"Knyga('{self.autorius}', '{self.pavadinimas}', {self.isleidimo_metai}, '{self.zanras}')"
+    
+class Biblioteka:
+    def __init__(self):
+        self.knygos = []
+        
+    def prideti_knyga(self):
+        autorius = input("Įveskite autoriaus vardą: ")
+        pavadinimas = input("Įveskite knygos pavadinimą: ")
+        isleidimo_metai = int(input("Įveskite išleidimo metus: "))
+        zanras = input("Įveskite žanrą: ")
+
+        nauja_knyga = Knyga(autorius, pavadinimas, isleidimo_metai, zanras)
+        self.knygos.append(nauja_knyga)
+        print(f"Knyga '{nauja_knyga.pavadinimas}' pridėta į biblioteką.")
+
+
+    def visos_knygos(self):
+        if not self.knygos:
+            print("knygu nera")
+        else:
+            print("\nVisos bibliotekos knygos:")
+            for knyga in self.knygos:
+                print(knyga)
+
+    def issaugoti_knygas(self, failo_pavadinimas):
+        with open(failo_pavadinimas, 'wb') as failas:
+            pickle.dump(self.knygos, failas)
+        print(f"Knygos išsaugotos faile '{failo_pavadinimas}'.")
+
+
+
+
+
